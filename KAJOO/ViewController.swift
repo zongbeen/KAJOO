55fbd1dd0d7be1f1b2e178526158e5e55ebbab62
zongbeen <hjb825@naver.com>
2022-09-25 16:23:10 +0900

    Initial Commit

---
+++ KAJOO/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KAJOO
+//
+//  Created by 한종빈 on 2022/09/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
