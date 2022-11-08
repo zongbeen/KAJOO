d7ad65d46ffd6497eccbcf8ef8a5d371300c34f4
zongbeen <hjb825@naver.com>
2022-11-08 21:05:57 +0900

    데이터베이스 수정

--- KAJOO/DataBase.swift
+++ KAJOO/DataBase.swift
@@ -9,19 +9,21 @@ import Foundation
 import RealmSwift
 
 class CartList: Object {
-    @objc dynamic var cartNum: String = ""
+    @objc dynamic var cartNum: Int = 0
     @objc dynamic var name: String = ""
     @objc dynamic var count: Int = 0
     @objc dynamic var cream: Int = 0
     @objc dynamic var shot: Int = 0
     @objc dynamic var price: Int = 0
+    @objc dynamic var cartLists: String = ""
     
     override static func primaryKey() -> String? {
         return "cartNum"
     }
 }
+
 class PayList: Object {
-    @objc dynamic var payNum: String = ""
+    @objc dynamic var payNum: Int = 0
     @objc dynamic var payname: String = ""
     @objc dynamic var payprice: Int = 0
 
