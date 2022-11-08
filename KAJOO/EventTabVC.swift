e9a587afbb2f99a578933a6093ab68f60debed41
zongbeen <hjb825@naver.com>
2022-11-08 21:06:26 +0900

    이벤트 배너 추가

--- KAJOO/EventTabVC.swift
+++ KAJOO/EventTabVC.swift
@@ -9,21 +9,29 @@ import UIKit
 
 class EventTabVC: UIViewController {
 
-    @IBOutlet weak var btn: UIButton!
-    @IBOutlet weak var EventLabel: UILabel!
-    @IBOutlet weak var button: UIButton!
+
+    @IBOutlet weak var eventBtn1: UIButton!
+    @IBOutlet weak var eventBtn2: UIButton!
+    @IBOutlet weak var eventBtn3: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        //self.navigationController?.navigationBar.isHidden = true
     }
     
-    @IBAction func didBtn(_ sender: Any) {
-        
-    }
-    
-    @IBAction func didTabButton(_ sender: Any) {
+    @IBAction func didTabEventBtn1(_ sender: Any) {
         if let url = URL(string: "http://www.naver.com"){
+            UIApplication.shared.open(url, options: [:])
+        }
+    }
+    
+    @IBAction func didTabEventBtn2(_ sender: Any) {
+        if let url = URL(string: "http://www.google.co.kr"){
+            UIApplication.shared.open(url, options: [:])
+        }
+    }
+    
+    @IBAction func didTabEventBtn3(_ sender: Any) {
+        if let url = URL(string: "http://www.daum.net"){
             UIApplication.shared.open(url, options: [:])
         }
     }
